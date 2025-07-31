f4229596fcea56820010f1098639299314ad3e38
okariyadids <iw.okariyadi@gmail.com>
2025-07-31 15:57:34 +0800

    Count Kill for selected year

--- WitchSaga/Controllers/HomeController.cs
+++ WitchSaga/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using WitchSaga.Models;
 
@@ -6,21 +7,28 @@ namespace WitchSaga.Controllers
 {
     public class HomeController : Controller
     {
+        #region Fields
+
         private readonly ILogger<HomeController> _logger;
+
+        #endregion
+
+        #region Constructor
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
+        #endregion
+
+        #region Public Methods
+
         public IActionResult Index()
         {
-            return View();
-        }
+            var model = new VillagerModel();
 
-        public IActionResult Privacy()
-        {
-            return View();
+            return View(model);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -28,5 +36,92 @@ namespace WitchSaga.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpPost]
+        public IActionResult CalculateKill(List<VillagerModel> villagers)
+        {
+            if (!villagers.Any())
+            {
+                return this.Json(new
+                {
+                    IsSuccess = false,
+                    ErrorMessage = "Empty Request",
+                });
+            }
+
+            if (villagers.Any(item => item.YearOfDeath <= 0 
+                || item.AgeOfDeath <= 0 
+                || item.YearOfDeath - item.AgeOfDeath <= 0))
+            {
+                return this.Json(new
+                {
+                    IsSuccess = true,
+                    Value = "-1",
+                });
+            }
+
+            var killResult = string.Empty;
+            var countTotalKill = 0;
+            var resultFormat = "Person {0} born on Year {1}, number of people killed on year {2} is {3}. ";
+
+            foreach (var villager in villagers) {
+                var selectedYear = villager.YearOfDeath - villager.AgeOfDeath;
+                var totalKilledPerson = selectedYear > 0 ? this.TotalKilledPersonInYear(selectedYear) : -1;
+
+                countTotalKill += totalKilledPerson;
+                killResult += string.Format(resultFormat, villager.Name, selectedYear, selectedYear, totalKilledPerson);
+            }
+
+            var averageResult = villagers.Count > 1 
+                ? string.Format("So the average is {4}", countTotalKill/villagers.Count) 
+                : string.Empty;
+
+            return this.Json(new
+            {
+                IsSuccess = true,
+                Value = string.Format("{0}{1}", killResult, averageResult),
+            });
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private int GetTotalFibonacciKill(int year)
+        {
+            if (year == 1)
+            {
+                return 1;
+            }
+
+            // For year >= 2
+            // Fibonacci is a sequence in which each element is the sum of the two elements that precede it.
+            // We need to assign the first 2 number
+            var fibonacciSquence = new List<int>();
+            fibonacciSquence.Add(0);
+            fibonacciSquence.Add(1);
+
+            var totalKill = 1;
+
+            for (int i = 2; i <= year; i++) { 
+                var nextSequence = fibonacciSquence[i - 1] + fibonacciSquence[i - 2];
+                fibonacciSquence.Add(nextSequence);
+                totalKill += nextSequence;
+            }
+
+            return totalKill;
+        }
+
+        private int TotalKilledPersonInYear(int selectedYear)
+        {
+            if (selectedYear <= 0)
+            {
+                return -1;
+            }
+
+            return this.GetTotalFibonacciKill(selectedYear);
+        }
+
+        #endregion
     }
 }
---
+++ WitchSaga/wwwroot/js/Index.js
@@ -0,0 +1,69 @@
+﻿(function ($, window, document) {
+    "use strict";
+
+	function initButton() {
+		var self = this;
+
+		$("#addVillager").on("click", function () {
+			var currentIndex = $(".villagerGroup").length;
+
+			var villagerGroup = `
+			<div class="villagerGroup mb-3">
+				<div>
+					<h5>Name</h5>
+					<input type="text" name="Villagers[${currentIndex}].Name" value="" />
+				</div>
+				<div>
+					<h5>Age of Death</h5>
+					<input type="number" name="Villagers[${currentIndex}].AgeOfDeath" value="0"/>
+				</div>
+				<div>
+					<h5>Year of Death</h5>
+					<input type="number" name="Villagers[${currentIndex}].YearOfDeath" value="0"/>
+				</div>
+			</div>
+		`;
+			$("#villagerContainer").append(villagerGroup);
+		});
+
+		// Handle calculate (submit)
+		$("#calculate").on("click", function () {
+			var formData = $("#villagerForm").serialize();
+			
+			$.ajax({
+				type: "POST",
+				url: self.FormDiv.data("calculate-url"),
+				data: formData,
+				success: function (result) {
+					debugger
+					if (result.isSuccess) {
+						$("#result").html(result.value);
+					}
+					else {
+						$("#result").html(result.errorMessage);
+					}
+				},
+				error: function (err) {
+					$("#result").html("An error occurred.");
+				}
+			});
+		});
+    }
+
+    var Index = function (element) {
+		this.Element = element;
+		this.FormDiv = $("#villagerForm");
+    };
+
+    Index.prototype = {
+        constructor: Index,
+        Register: function () {
+			var self = this;
+
+			initButton.call(self);
+        }
+    };
+
+    // export javascript class into window environment
+    window.Index = Index;
+})(jQuery, window, document);
